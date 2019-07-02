38689b46f67113c618c4d9dc073ae29773156a95
Majo <you@example.com>
2019-07-01 23:24:33 -0500

    primera version

---
+++ Ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+ public class Ejercicio1{
+  public static void main(String[] args){
+  float num,den,mun1,den2;
+  Scanner lee=new Scanner(System.in);
+  System.out.println("Ingrese el numerador");
+  num=lee.nextFloat();
+  System.out.println("Ingrese el denominador");
+  den=lee.nextFloat();
+  System.out.println("Ingrese el numerador 1");
+  num=lee.nextFloat();
+  System.out.println("Ingrese el denominador 2");
+  den=lee.nextFloat();
+}
+}
